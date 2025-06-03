132d61051422905d7fa6f2b875c8c5983dd9aa1b
Coby Eastwood <coby.eastwood@sentry.io>
2025-06-03 15:54:53 -0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,14 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./i18n/requests.js");
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-const nextIntlConfig = withNextIntl(nextConfig);
-
-export default withSentryConfig(nextIntlConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
+export default withSentryConfig(nextConfig, {
+  org: "sentry-coby-us",
+  project: "javascript-nextjs-t4",
   silent: !process.env.CI,
   authToken: process.env.SENTRY_AUTH_TOKEN,
-  widenClientFileUpload: false,
-  hideSourceMaps: true,
-  automaticVercelMonitors: true,
+  widenClientFileUpload: true,
 });
